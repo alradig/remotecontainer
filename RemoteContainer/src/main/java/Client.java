9684862bc2843e539b229a4b7974decc14c3c7e9
alradig <51677083+alradig@users.noreply.github.com>
2020-03-25 10:37:22 +0100

    M1(andre) (#1)
    * testing
    
    * removed testingM1 file
    
    * testing M1 view with Mika
    
    * Deleted file to refresh Mikas view of M1
    
    * New feature
    
    * it works !
    
    * sss
    
    * First try
    
    * feature adjusted.
    
    * Worked on RegisterClient feature.
    
    * Worked on Register Client feature
    
    * Created new RegisterClient feature file
    
    * Worked on RegisterClient2.feature
    
    * Register client feature development
    
    * Everything changed
    
    * Moved class files to proper location
    
    * cleaned step definition
    
    * new feature: update client information
    
    * feature: update client information
    
    * update client information feature
    
    * feature: update client information
    
    * updated steps definitions
    
    * developed feature Update client information
    
    Co-authored-by:  <jacobhofestaedt@web.de>

---
+++ RemoteContainer/src/main/java/Client.java
@@ -0,0 +1,45 @@
+
+public class Client {
+ String name;
+ String email;
+ String pw;
+ String refPerson;
+ String adress;
+ boolean Register;
+public String getName() {
+	return name;
+}
+public boolean isRegister() {
+	return Register;
+}
+public void setRegister(boolean register) {
+	Register = register;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPw() {
+	return pw;
+}
+public void setPw(String pw) {
+	this.pw = pw;
+}
+public String getRefPerson() {
+	return refPerson;
+}
+public void setRefPerson(String refPerson) {
+	this.refPerson = refPerson;
+}
+public String getAdress() {
+	return adress;
+}
+public void setAdress(String adress) {
+	this.adress = adress;
+}
+}
--- RemoteContainer/src/test/java/StepsDefinition.java
+++ RemoteContainer/src/test/java/StepsDefinition.java
@@ -1,4 +1,77 @@
-
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static org.junit.Assert.*;
 public class StepsDefinition {
+	Client client = new Client();
+	@Given("I have chosen to register a client")
+	public void I_have_chosen_to_register_a_client(){
+	   client.setRegister(true);
+	   
+	}
 
+	@Given("I am about to enter the client information")
+	public void I_am_about_to_enter_the_client_information() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I do not enter the clients name")
+	public void i_do_not_enter_the_clients_name() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I do not enter the clients email")
+	public void i_do_not_enter_the_clients_email() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("I do not enter the clients address")
+	public void i_do_not_enter_the_clients_address() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I do not enter the clients reference person")
+	public void i_do_not_enter_the_clients_reference_person() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I submit the form")
+	public void I_submit_the_form() {
+
+	}
+
+	@Then("the form should be redisplayed")
+	public void the_form_should_be_redisplayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Then("I should be asked if I want to submit it anyway")
+	public void i_should_be_asked_if_I_want_to_submit_it_anyway() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("I should see a message advising me that the clients email is required")
+	public void i_should_see_a_message_advising_me_that_the_clients_email_is_required() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I should see a message advising me that the clients address is missing")
+	public void i_should_see_a_message_advising_me_that_the_clients_address_is_missing() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I should see a message advising me that the clients reference person is missing")
+	public void i_should_see_a_message_advising_me_that_the_clients_reference_person_is_missing() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
 }
