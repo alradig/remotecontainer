26dcf250bfb340cb9ab3db1b1742b6623115d19d
JMH <jacobhofestaedt@web.de>
2020-03-29 13:27:03 +0200

    trimmed register feature

--- RemoteContainer/src/test/java/StepsDefinition.java
+++ RemoteContainer/src/test/java/StepsDefinition.java
@@ -116,71 +116,11 @@ public class StepsDefinition {
 	    this.clientForm.setPwField(clientInfo.get(1).get(5)); 	
 
 	}
-	
-	@When("I do not enter the clients name")
-	public void i_do_not_enter_the_clients_name() {
-	    if ((client.getName()).isEmpty()) {
-	    	response.setErrorMessage("You haven't set a name.");
-	    }
-	}
-
-	@When("I do not enter the clients email")
-	public void i_do_not_enter_the_clients_email() {
-		if ((client.getEmail()).isEmpty()) {
-	    	response.setErrorMessage("You haven't set an email.");
-		 }
-			}
-	@When("I do not enter the clients address")
-	public void i_do_not_enter_the_clients_address() {
-		if ((client.getAddress()).isEmpty()) {
-	    	response.setErrorMessage("You haven't set an adress.");
-		 }
-			}
-
-	@When("I do not enter the clients reference person")
-	public void i_do_not_enter_the_clients_reference_person() {
-		if ((client.getRefPerson()).isEmpty()) {
-	    	response.setErrorMessage("You haven't set a ref person");
-		 }
-			}
-	
-
 	@When("I submit the form")
 	public void I_submit_the_form() {
 		response = clientForm.submit();
 	}
 
-	@Then("the form should be redisplayed")
-	public void the_form_should_be_redisplayed() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-	
-	@Then("I should be asked if I want to submit it anyway")
-	public void i_should_be_asked_if_I_want_to_submit_it_anyway() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-	
-	@Then("I should see a message advising me that the clients name is required")
-	public void i_should_see_a_message_advising_me_that_the_clients_name_is_required() {
-		assertEquals(response.getErrorMessage(),"You haven't set a name.");
-	}
-		
-	@Then("I should see a message advising me that the clients email is required")
-	public void i_should_see_a_message_advising_me_that_the_clients_email_is_required() {
-		assertEquals(response.getErrorMessage(),"You haven't set an email.");
-	}
-
-	@Then("I should see a message advising me that the clients address is missing")
-	public void i_should_see_a_message_advising_me_that_the_clients_address_is_missing() {
-		assertEquals(response.getErrorMessage(),"You haven't set an adress.");
-	}
-
-	@Then("I should see a message advising me that the clients reference person is missing")
-	public void i_should_see_a_message_advising_me_that_the_clients_reference_person_is_missing() {
-		assertEquals(response.getErrorMessage(),"You haven't set a ref person");
-	}
 	/*
 	 * Step Definitions for Update a clients information
 	 */
