a1fbb9d9124875303a10a22a1fdbd8b0d95f702c
jiayi0107@gmail.com <jiayi0107@gmail.com>
2020-04-22 16:26:22 +0200

    More

--- RemoteContainer/src/main/java/LogisticCompany/gui/RegisterClientScreen.java
+++ RemoteContainer/src/main/java/LogisticCompany/gui/RegisterClientScreen.java
@@ -6,7 +6,9 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import LogisticCompany.App.LogisticCompanyApp;
 
@@ -14,7 +16,20 @@ public class RegisterClientScreen {
 	LogisticCompanyApp logisticCompanyApp;
 	private LogisticCompanyFunctionalitiesScreen parentWindow;
 	private JPanel panelRegisterClient;
-	
+	private JTextField nameField;
+	private JLabel lblName;
+	private JTextField emailField;
+	private JLabel lblemail;
+	private JTextField passwordField;
+	private JLabel lblPassword;
+	private JTextField zipCpdeField;
+	private JLabel lblZipCode;
+	private JTextField cityField;
+	private JLabel lblCity;
+	private JTextField streetField;
+	private JLabel lblStreet;
+	private JTextField refPField;
+	private JLabel lblRefP;
 
 	public RegisterClientScreen(LogisticCompanyApp logisticCompanyApp, LogisticCompanyFunctionalitiesScreen parentWindow) {
 		this.logisticCompanyApp = logisticCompanyApp;
@@ -29,6 +44,85 @@ public class RegisterClientScreen {
 		panelRegisterClient.setBorder(BorderFactory.createTitledBorder(
                 "Register Client"));
 		
+		nameField = new JTextField();
+		nameField.setBounds(138, 100, 130, 26);
+		panelRegisterClient.add(nameField);
+		nameField.setColumns(10);
+		
+		lblName = new JLabel("Name:");
+		lblName.setBounds(50, 105, 74, 16);
+		panelRegisterClient.add(lblName);
+		
+		emailField = new JTextField();
+		emailField.setBounds(138, 140, 130, 26);
+		panelRegisterClient.add(emailField);
+		nameField.setColumns(10);
+		
+		lblemail = new JLabel("email:");
+		lblemail.setBounds(50, 145, 74, 16);
+		panelRegisterClient.add(lblemail);
+		
+		passwordField = new JTextField();
+		passwordField.setBounds(138, 180, 130, 26);
+		panelRegisterClient.add(passwordField);
+		passwordField.setColumns(10);
+		
+		lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(50, 185, 74, 16);
+		panelRegisterClient.add(lblPassword);
+		
+		JLabel lblAddress = new JLabel("Address");
+		lblAddress.setBounds(170, 225, 74, 16);
+		panelRegisterClient.add(lblAddress);
+		
+		zipCpdeField = new JTextField();
+		zipCpdeField.setBounds(138, 260, 130, 26);
+		panelRegisterClient.add(zipCpdeField);
+		passwordField.setColumns(10);
+		
+		lblZipCode = new JLabel("Zip code:");
+		lblZipCode.setBounds(50, 265, 74, 16);
+		panelRegisterClient.add(lblZipCode);
+		
+		cityField = new JTextField();
+		cityField.setBounds(138, 300, 130, 26);
+		panelRegisterClient.add(cityField);
+		passwordField.setColumns(10);
+		
+		lblCity = new JLabel("City:");
+		lblCity.setBounds(50, 305, 74, 16);
+		panelRegisterClient.add(lblCity);
+		
+		streetField = new JTextField();
+		streetField.setBounds(138, 340, 130, 26);
+		panelRegisterClient.add(streetField);
+		passwordField.setColumns(10);
+		
+		lblStreet = new JLabel("Street:");
+		lblStreet.setBounds(50, 345, 74, 16);
+		panelRegisterClient.add(lblStreet);
+		
+		refPField = new JTextField();
+		refPField.setBounds(138, 380, 130, 26);
+		panelRegisterClient.add(refPField);
+		passwordField.setColumns(10);
+		
+		lblRefP = new JLabel("Reference Person:");
+		lblRefP.setBounds(50, 385, 74, 16);
+		panelRegisterClient.add(lblRefP);
+		
+		
+		JButton btnregister = new JButton("Register");
+		btnregister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// register and go back 
+				parentWindow.setVisible(true);
+			}
+		});
+		btnregister.setBounds(170, 425, 74, 16);
+		panelRegisterClient.add(btnregister);
+		
+				
 		JButton btnBack = new JButton("Back");
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
--- RemoteContainer/src/main/java/LogisticCompany/gui/RegisterJourneyScreen.java
+++ RemoteContainer/src/main/java/LogisticCompany/gui/RegisterJourneyScreen.java
@@ -1,25 +1,84 @@
 package LogisticCompany.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import LogisticCompany.App.LogisticCompanyApp;
 
 public class RegisterJourneyScreen {
 	LogisticCompanyApp logisticCompanyApp;
 	private ClientFunctionalitiesScreen parentWindow;
-	private JPanel panelRegisterClient;
+	private JPanel panelRegisterJourney;
 	
+	private JTextField cargoField;
+	private JLabel lblCargo;
+	private JTextField startDestinationField;
+	private JLabel lblStartDestinationField;
+	private JTextField endDestinationField;
+	private JLabel lblEndestinationField;
 
+	
 	public RegisterJourneyScreen(LogisticCompanyApp logisticCompanyApp, ClientFunctionalitiesScreen parentWindow) {
 		this.logisticCompanyApp = logisticCompanyApp;
 		this.parentWindow = parentWindow;
-//		initialize();
+		initialize();
+	}
+	private void initialize() {
+		panelRegisterJourney = new JPanel();
+		parentWindow.addPanel(panelRegisterJourney);
+		panelRegisterJourney.setLayout(null);
+		panelRegisterJourney.setBorder(BorderFactory.createTitledBorder(
+                "Register Client"));
+		
+		cargoField = new JTextField();
+		cargoField.setBounds(138, 100, 130, 26);
+		panelRegisterJourney.add(cargoField);
+		cargoField.setColumns(10);
+		
+		lblCargo = new JLabel("Cargo:");
+		lblCargo.setBounds(50, 105, 74, 16);
+		panelRegisterJourney.add(lblCargo);
+		
+		startDestinationField = new JTextField();
+		startDestinationField.setBounds(138, 140, 130, 26);
+		panelRegisterJourney.add(startDestinationField);
+		startDestinationField.setColumns(10);
+		
+		lblStartDestinationField = new JLabel("Start destination:");
+		lblStartDestinationField.setBounds(50, 145, 74, 16);
+		panelRegisterJourney.add(lblStartDestinationField);
+		
+		endDestinationField = new JTextField();
+		endDestinationField.setBounds(138, 180, 130, 26);
+		panelRegisterJourney.add(endDestinationField);
+		endDestinationField.setColumns(10);
+		
+		lblEndestinationField = new JLabel("End destination:");
+		lblEndestinationField.setBounds(50, 185, 74, 16);
+		panelRegisterJourney.add(lblEndestinationField);
+		
+		
+		JButton btnregister = new JButton("Register");
+		btnregister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// register and go back 
+				parentWindow.setVisible(true);
+			}
+		});
+		btnregister.setBounds(170, 425, 74, 16);
+		panelRegisterJourney.add(btnregister);
+		
 	}
 	
-	
 	public void setVisible(boolean aFlag) {
-		panelRegisterClient.setVisible(aFlag);
+		panelRegisterJourney.setVisible(aFlag);
 		
 	}
 
