0f646e9a1a4795af991444edb6bf54283c334ebf
IdaVillumsen <s154412@student.dtu.dk>
2020-04-25 10:22:07 +0200

    bva again

--- RemoteContainer/src/main/java/LogisticCompany/gui/ClientPrinter.java
+++ RemoteContainer/src/main/java/LogisticCompany/gui/ClientPrinter.java
@@ -12,9 +12,10 @@ public class ClientPrinter {
 
 	public String printDetail() {
 		StringBuffer b = new StringBuffer();
-		b.append(String.format("<b>Client name:</b>     %s<br>", client.getName()));
+		b.append(String.format("<html>+<b>Client name:</b>     %s<br>", client.getName()));
 		b.append(String.format("<b>E-mail:</b>    %s<br>", client.getEmail()));
-		b.append(String.format("<b>Reference person:</b> %s<br>", client.getReference_person()));
+		b.append(String.format("<b>Address:</b>    %s<br>", client.getAddress()));
+		b.append(String.format("<b>Reference person:</b> %s<br></html>", client.getReference_person()));
 		return b.toString();
 	}
 	
