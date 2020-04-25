fa9f89f8d5a05738ea19e0c5fccfd2e34c4efff5
jiayi0107@gmail.com <jiayi0107@gmail.com>
2020-04-25 09:51:22 +0200

    added client search Journey

---
+++ RemoteContainer/src/main/java/LogisticCompany/gui/JourneyPrinter.java
@@ -0,0 +1,25 @@
+package LogisticCompany.gui;
+
+import LogisticCompany.info.JourneyInfo;
+
+public class JourneyPrinter {
+	
+	private JourneyInfo journey;
+
+	public JourneyPrinter(JourneyInfo journey) {
+		this.journey = journey;
+	}
+
+	public String printDetail() {
+		StringBuffer b = new StringBuffer();
+		b.append(String.format("<b>The journey has content:</b>     %s<br>", journey.getCargo()));
+		b.append(String.format("<b>The journey starts at:</b>    %s<br>", journey.getOriginPort()));
+		b.append(String.format("<b>The journey ends at:</b>    %s<br>" , journey.getDestinationPort()));
+		return b.toString();
+	}
+	
+	public String print() {
+			return "Journey from " + journey.getOriginPort() + " to " +journey.getDestinationPort();
+	}
+
+}
