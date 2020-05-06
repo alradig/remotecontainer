673ee18405e99f4732f0e16d799ee943570c9913
jiayi0107@gmail.com <jiayi0107@gmail.com>
2020-05-06 11:13:06 +0200

    one more comment

--- RemoteContainer/src/main/java/LogisticCompany/App/OperationNotAllowedException.java
+++ RemoteContainer/src/main/java/LogisticCompany/App/OperationNotAllowedException.java
@@ -2,8 +2,8 @@ package LogisticCompany.App;
 
 
 /**
- * 
- *
+ * This class represents an exception that will be thrown when the operation is not allowed
+ * for instance when the client is logged in and tries to perform an action that requires logistic company authorization 
  */
 public class OperationNotAllowedException extends Exception {
 
